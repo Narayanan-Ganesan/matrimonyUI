a9f407b90cd06441df235aadd6e671e09689c2aa
GayathriRangasamy <47479026+GayathriRangasamy@users.noreply.github.com>
2023-07-12 15:21:48 +0530

    Profile Page

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../user.model';
+
+
 
 @Component({
   selector: 'app-main',
@@ -7,9 +10,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainComponent implements OnInit {
 
+  user:User={full_name:'Sanchana',age:24,father_name:'Soundarrajan',dob:
+  new Date('2022-06-15'),reg_date:new Date('2022-06-15'),district:'COIMBATORE',property_details:'10 CR',
+  horoscope:'CANCER',star:'Cancer',occupation:'Software Engineer Bangalore',
+  image:'https://images.pexels.com/photos/1925482/pexels-photo-1925482.jpeg?auto=compress&cs=tinysrgb&w=600'};
+
   constructor() { }
 
   ngOnInit(): void {
+     // this.user=[
+      
+    //   {full_name:'Sanchana',age:24,father_name:'XXX',dob:
+    //   new Date('2022-06-15'),reg_date:new Date('2022-06-15'),district:'COIMBATORE',property_details:'10 CR',
+    //   horoscope:'CANCER',star:'',occupation:'SOFTWARE ENGINEER'},
+      
+    // ];
   }
 
 }
---
+++ src/app/user.model.ts
@@ -0,0 +1,15 @@
+export interface User {
+    full_name: string;
+    age: number;
+    father_name:string;
+    dob:Date;
+    reg_date:Date;
+    district:string;
+    property_details:string;
+    horoscope:string;
+    star:string;
+    occupation:string;
+    image: string;
+
+  }
+  
