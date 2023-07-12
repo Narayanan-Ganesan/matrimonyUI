4078f3bb367c2336a91aef3b1babfcbfc762d6b8
GayathriRangasamy <47479026+GayathriRangasamy@users.noreply.github.com>
2023-07-12 15:23:04 +0530

    User Information

--- src/app/user-information/user-information.component.ts
+++ src/app/user-information/user-information.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-user-information',
   templateUrl: './user-information.component.html',
   styleUrls: ['./user-information.component.less']
 })
 export class UserInformationComponent implements OnInit {
+  
+  registerForm!: FormGroup;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+
+    this.registerForm = this.formBuilder.group({});
+   
   }
 
 }
+
