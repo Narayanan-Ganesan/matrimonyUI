5816ad993e651544d583470993013399853bb144
GayathriRangasamy <47479026+GayathriRangasamy@users.noreply.github.com>
2023-07-15 13:28:30 +0530

    User Information Form
    Adding rest of the form components

--- src/app/user-information/user-information.component.ts
+++ src/app/user-information/user-information.component.ts
@@ -7,14 +7,37 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class UserInformationComponent implements OnInit {
   
+  submitted!:boolean;
   registerForm!: FormGroup;
 
   constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
 
-    this.registerForm = this.formBuilder.group({});
-   
+    this.registerForm = this.formBuilder.group({
+      registerNo: ['', {updateOn: 'change',validators: [Validators.required] }],
+      firstName: [''],
+      // Add other form controls and validators
+    });
+  }
+
+
+  selectedImage: string | undefined;
+
+  handleFileInput(event: any) {
+    const file = event.target.files[0];
+    this.getBase64(file).then((result: string | ArrayBuffer | null) => {
+      this.selectedImage = result as string;
+    });
+  }
+
+  getBase64(file: File): Promise<string | ArrayBuffer | null> {
+    return new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      reader.onload = () => resolve(reader.result);
+      reader.onerror = (error) => reject(error);
+      reader.readAsDataURL(file);
+    });
   }
 
 }
