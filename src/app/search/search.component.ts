6dc132dfe6bf73bb539de2c37ccdac9fa5f7516d
GayathriRangasamy <47479026+GayathriRangasamy@users.noreply.github.com>
2023-07-17 22:01:47 +0530

    Advanced Search Form

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-search',
@@ -7,9 +8,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchComponent implements OnInit {
 
-  constructor() { }
+  advancesearchform!:FormGroup;
+
+  constructor(private formbuilder:FormBuilder) { }
 
   ngOnInit(): void {
+
+    this.advancesearchform=this.formbuilder.group(
+      {
+        gender: [''],
+        agefrom:[''],
+        ageTo:[''],
+        horoscope:[''],
+        recentprofile:[''],
+        district:[''],
+        job:[''],
+
+        martialstatus:['']
+
+      }
+    );
   }
+
 
 }
